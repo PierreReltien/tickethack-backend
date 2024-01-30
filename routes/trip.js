08ec5fda7ebbcf902fedd1a2190aa15b8c32c7c0
RELTIEN <pierre.reltien@gmail.com>
2024-01-30 17:42:05 +0100

    ajout de fonctionnalités sur trip.js

--- routes/trip.js
+++ routes/trip.js
@@ -5,15 +5,29 @@ const mongoose = require('mongoose')
 
 const fetch = require('node-fetch');
 const Trip = require('../models/trips');
+const moment = require('moment')
 
 
 router.get('/:departure/:arrival/:date', (req, res) => {
 
+let date = new Date(req.params.date)
+console.log(date)
+
+let beginning = moment(date).startOf('day')
+let ending = moment(date).endOf('day')
+
+console.log(beginning);
+console.log(ending);
+
+
+
 	Trip.find({
-		"departure": req.params.departure, 
-		"arrival" : req.params.arrival, 
-		"date": req.params.date})
+		departure: req.params.departure, 
+		arrival : req.params.arrival, 
+		date: {$gt : beginning, $lt : ending}})
+
 	.then(data => {
+		console.log(data)
 
 		if(data.length>0) {
 			res.json({ result: true, trip : data })
